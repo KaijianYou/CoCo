7f7510d421f79fdb43756d6e6d05e00d676d4367
KaijianYou <kaijianyou@gmail.com>
2018-06-02 21:01:36 +0800

    feat: 整理项目包依赖，以及修正一些拼写错误

--- blog/const/__init__.py
+++ blog/const/__init__.py
@@ -4,8 +4,3 @@ from .constant import Constant
 def init_const(config_env):
     pass
 
-
-__all__ = [
-    'init_const',
-    'Constant'
-]
--- blog/const/constant.py
+++ blog/const/constant.py
@@ -2,12 +2,12 @@ class Constant:
     class _ConstantError(TypeError):
         pass
 
-    class _CosntantCaseError(_ConstantError):
+    class _ConstantCaseError(_ConstantError):
         pass
 
     def __setattr__(self, key, value):
         if key in self.__dict__:
             raise self._ConstantError(f'Cannot change constant {key}')
         if not key.isupper():
-            raise self._ConstantCaseError(f'Constant name {key} is not alll uppercase')
+            raise self._ConstantCaseError(f'Constant name {key} is not all uppercase')
         self.__dict__[key] = value
